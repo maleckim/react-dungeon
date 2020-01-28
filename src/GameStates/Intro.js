b9b72db961bc5f69eb27fdc1e1ac360991db02e6
tomilone <tonymilone.tech@gmail.com>
2020-01-28 00:08:34 -0600

    Styling Intro

--- src/GameStates/Intro.js
+++ src/GameStates/Intro.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import spook from '../MapDrawing/spookycave.png'
 
 export default function Intro(){
 
   return(
-    <>
-    <h1>Now begins your great Journey!</h1>
-    <p>press <Link to="/charSelect">Start</Link> to begin!!</p>
-    </>
+    <div className='introContainer'>
+      <h1>Now begins your great Journey!</h1>
+      <img className='spookyCave' src={spook}></img>
+      <p>press <Link className='start' to="/charSelect">Start</Link> to begin!!</p>
+    </div>
   )
 }
