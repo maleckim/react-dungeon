689bc0ed096491e35ca6e7828f1c4e2cceaf588b
maleckim <maleckimatthewm@gmail.com>
2020-01-27 21:17:18 -0600

    dah

---
+++ src/GameStates/Fight.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ActionBar from '../UI/ActionBar'
+
+
+export default class Fight extends React.Component{
+
+  render(){
+    return(
+      <p>hey</p>
+      
+    )
+  }
+}
